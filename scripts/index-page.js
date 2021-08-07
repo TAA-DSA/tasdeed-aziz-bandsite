c3fbe2d23e7db73d05b796e89188c6b181b24714
Tasdeed Aziz <ayonaziz@gmail.com>
2021-08-07 17:15:38 -0400

    script update

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,2 +1,16 @@
 //Script Test//
 console.log("Hello to bandSite");
+
+const subForm = document.querySelector('#comment__field');
+
+subForm.addEventListener('submit',function(event){
+    event.preventDefault();
+
+    const user_name = event.target.name.value;
+    console.log(user_name);
+
+    const comment_entry = event.target.sentiments.value;
+    console.log(comment_entry);
+
+    
+})
