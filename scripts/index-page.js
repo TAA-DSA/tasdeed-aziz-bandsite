4e5c1718ec5201634e141bd15f2470d9e2e53de6
Tasdeed Aziz <ayonaziz@gmail.com>
2021-08-08 19:49:30 -0400

    click and div

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -5,27 +5,54 @@ console.log("Hello to bandSite");
 
 const comForm = document.querySelector('#comment__btn');
 
+// const review__header = document.getElementById('review__header');
+// const pText = document.getElementById('preview__text');
+
+
+
 
 comForm.addEventListener('click',function(event){
     event.preventDefault();
-
-    const user_name = document.getElementById('name').value;
-    console.log(user_name);
-
-    const comment_entry = document.getElementById('sentiments').value;
-    console.log(comment_entry);
-
-    const reviewHeader = document.createElement('h2');
+    // const user_name = document.getElementById('name').value;
+    // const comment_entry = document.getElementById('sentiments').value; 
+    // review__header.innerHTML = user_name;
+    // pText.innerHTML = comment_entry;
     
+
+    
+    
+
+
+    // document.getElementById('#review__header').innerText = user_name;
+
+   
+    // console.log(comment_entry);
+   
+
+    // const reviewHeader = document.createElement('h2');
+    // reviewHeader.innerText = user_name
+
+    // const reviewText  = document.createElement('p');
+    // // console.log(reviewText);
 
     const divContainer = document.getElementById('review__input');
 
 
     const newDiv = document.createElement('div');
     newDiv.classList.add('review');
-    console.log(user_name);
     divContainer.appendChild(newDiv);
 
+    
+
+    
+
+    
+
+    
+    
+    
+
+    
 
     
 })
