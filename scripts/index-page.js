28e116b5a4d91ef9bf3954fb50838f69ccfdc49f
Tasdeed Aziz <ayonaziz@gmail.com>
2021-08-08 17:04:32 -0400

    script element created

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -3,21 +3,28 @@ console.log("Hello to bandSite");
 
 //initiating Eventlistner//
 
-const comForm = document.querySelector('#comment__field');
+const comForm = document.querySelector('#comment__btn');
 
-comForm.addEventListener('submit',function(event){
+
+comForm.addEventListener('click',function(event){
     event.preventDefault();
 
-    const user_name = event.target.name.value;
+    const user_name = document.getElementById('name').value;
     console.log(user_name);
 
-    const comment_entry = event.target.sentiments.value;
+    const comment_entry = document.getElementById('sentiments').value;
     console.log(comment_entry);
+
+    const reviewHeader = document.createElement('h2');
+    
+
+    const divContainer = document.getElementById('review__input');
+
 
     const newDiv = document.createElement('div');
     newDiv.classList.add('review');
     console.log(user_name);
-    document.body.appendChild(newDiv);
+    divContainer.appendChild(newDiv);
 
 
     
