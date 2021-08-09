5ca368df29b1c58fcffd612544318829b8c03150
Tasdeed Aziz <ayonaziz@gmail.com>
2021-08-09 09:20:48 -0400

    comment section script

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -4,21 +4,89 @@ console.log("Hello to bandSite");
 //initiating Eventlistner//
 
 const comForm = document.querySelector('#comment__btn');
-
-// const review__header = document.getElementById('review__header');
-// const pText = document.getElementById('preview__text');
-
+const parentDiv = document.querySelector('.review__container');
+console.log(parentDiv);
+const getDate = new Date();
+const date = (getDate.getMonth()+ 1) + "/" + getDate.getDate() + "/" + getDate.getFullYear()
 
 
 
 comForm.addEventListener('click',function(event){
     event.preventDefault();
-    // const user_name = document.getElementById('name').value;
-    // const comment_entry = document.getElementById('sentiments').value; 
-    // review__header.innerHTML = user_name;
-    // pText.innerHTML = comment_entry;
+       
+    
+   const newDiv = document.createElement('div');
+   newDiv.classList.add('review__cornor');
+   parentDiv.appendChild(newDiv);
+
+   const childDiv = document.createElement('div');
+   newDiv.appendChild(childDiv);
+
+   const imgDiv = document.createElement('img');
+   imgDiv.classList.add("image_placeholder");
+   childDiv.appendChild(imgDiv);
+
+   const anotherDiv = document.createElement('div');
+   anotherDiv.classList.add("reviews");
+   newDiv.appendChild(anotherDiv);
+
+   const dateDiv = document.createElement('div');
+   newDiv.appendChild(dateDiv);
+
+   const hName = document.createElement("h2"); 
+   hName.classList.add('h_title');
+   anotherDiv.appendChild(hName);
+   
+   const pText = document.createElement("p");
+   pText.classList.add("user__comments");
+   anotherDiv.appendChild(pText);
+
+   const pTime = document.createElement("p");
+   pTime.classList.add("date");
+   dateDiv.appendChild(pTime);
+
+   const user_name = document.getElementById('name').value;
+   const comment_entry = document.getElementById('sentiments').value; 
+   hName.innerText = user_name;
+   pText.innerText = comment_entry;
+   
+
+
     
 
+//     const dTime = document.createElement("div");
+//     dTime.classList.add("day__time");
+//     test.appendChild(dTime);
+//     console.log(dTime);
+    
+//     dTime.innerText = date;
+    
+   
+
+    // const imgPlaceholder = document.createElement("div");
+    // imgPlaceholder.classList.add("image_placeholder");
+    // test.appendChild(imgPlaceholder);
+
+    
+
+    
+    
+
+
+    
+
+    
+
+
+    
+
+
+
+
+
+    
+
+ 
     
     
 
@@ -33,26 +101,7 @@ comForm.addEventListener('click',function(event){
     // reviewHeader.innerText = user_name
 
     // const reviewText  = document.createElement('p');
-    // // console.log(reviewText);
+   // // console.log(reviewText);
 
-    const divContainer = document.getElementById('review__input');
-
-
-    const newDiv = document.createElement('div');
-    newDiv.classList.add('review');
-    divContainer.appendChild(newDiv);
-
-    
-
-    
-
-    
-
-    
-    
-    
-
-    
-
-    
+  
 })
